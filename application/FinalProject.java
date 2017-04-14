af6233b65df53ecf0cea48c94964bf0024a5beea
Alex Kassil <alexkassil@sbcglobal.net>
2017-04-14 15:51:13 -0700

    Updated Course Testings

--- application/FinalProject.java
+++ application/FinalProject.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class FinalProject {
 
 	public static void main(String[] args) {
-		Assignment assignment1 = new Assignment();
-		Assignment assignment2 = new Assignment(new Date(), 20, "QUIZ 1", assignmentType.QUIZ);
-		Assignment assignment3 = new Assignment(100, "Test 1", assignmentType.TEST);
+		Assignment assignment1 = new Assignment(1);
+		Assignment assignment2 = new Assignment(2, new Date(), 20, "QUIZ 1", assignmentType.QUIZ);
+		Assignment assignment3 = new Assignment(3, 100, "Test 1", assignmentType.TEST);
 		Assignment[] assignments = {assignment1, assignment2, assignment3};
 		
 		StudentRecord sr1 = new StudentRecord("Charles Smith", 000);
@@ -15,7 +15,7 @@ public class FinalProject {
 		sr2.setAssignments(Arrays.asList(assignments));
 		
 		StudentRecord sr3 = new StudentRecord("George MacDonald", 002);
-		sr3.addAssignment(new Assignment(50, "QUIZ 5", assignmentType.QUIZ));
+		sr3.addAssignment(new Assignment(4, 50, "QUIZ 5", assignmentType.QUIZ));
 		
 		StudentRecord[] records = {sr1, sr2, sr3};
 		
