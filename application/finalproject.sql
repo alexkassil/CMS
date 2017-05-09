acff9a8fa01378b48f501894f160194f0ecdf387
Alex Kassil <alexkassil@sbcglobal.net>
2017-05-09 12:02:04 -0700

    Add Grades entries for three student records

--- application/finalproject.sql
+++ application/finalproject.sql
@@ -48,5 +48,25 @@ insert into Assignments values(4, 'Math 54', now(), 20, 'Quiz 2', 'QUIZ');
 insert into Assignments values(5, 'Math 54', now(), 20, 'Test 1', 'TEST');
 insert into Assignments values(6, 'Math 54', now(), 20, 'Test 2', 'TEST');
 
+insert into Grades values('Math 54', 1, 1, 1.5);
+insert into Grades values('Math 54', 1, 2, 1);
+insert into Grades values('Math 54', 1, 3, 0);
+insert into Grades values('Math 54', 1, 4, .9);
+insert into Grades values('Math 54', 1, 5, 1.0001);
+insert into Grades values('Math 54', 1, 6, .949542);
+
+insert into Grades values('Math 54', 2, 1, .95);
+insert into Grades values('Math 54', 2, 2, .5432);
+insert into Grades values('Math 54', 2, 3, 1.01);
+insert into Grades values('Math 54', 2, 4, 1.1);
+insert into Grades values('Math 54', 2, 5, .43);
+insert into Grades values('Math 54', 2, 6, 1.5);
+
+insert into Grades values('Math 54', 3, 1, 1.5);
+insert into Grades values('Math 54', 3, 2, 1.0);
+insert into Grades values('Math 54', 3, 3, .94);
+insert into Grades values('Math 54', 3, 4, .67);
+insert into Grades values('Math 54', 3, 5, .8787878);
+insert into Grades values('Math 54', 3, 6, .444444444);
 
 commit;
