0c8d64d390e4f7a6caefe4acf20e5c3fb8cf3ad9
Alex Kassil <alexkassil@sbcglobal.net>
2017-05-09 14:34:13 -0700

    Add Comp 232 Course and records and gradesXSXC

--- application/finalproject.sql
+++ application/finalproject.sql
@@ -69,4 +69,40 @@ insert into Grades values('Math 54', 3, 4, .67);
 insert into Grades values('Math 54', 3, 5, .8787878);
 insert into Grades values('Math 54', 3, 6, .444444444);
 
+insert into Courses values('Comp 232', .5, .2, .3);
+
+insert into StudentRecords values(4, 'Comp 232', 'Matt');
+insert into StudentRecords values(5, 'Comp 232', 'Claire');
+insert into StudentRecords values(6, 'Comp 232', 'Jackie');
+
+insert into Assignments values(7, 'Comp 232', now(), 50, 'Homework 1', 'HWRK');
+insert into Assignments values(8, 'Comp 232', now(), 100, 'Homework 2', 'HWRK');
+insert into Assignments values(9, 'Comp 232', now(), 100, 'Homework 3', 'HWRK');
+insert into Assignments values(10, 'Comp 232', now(), 100, 'Homework 4', 'HWRK');
+insert into Assignments values(11, 'Comp 232', now(), 20, 'Quiz 1', 'QUIZ');
+insert into Assignments values(12, 'Comp 232', now(), 200, 'Final', 'TEST');
+
+insert into Grades values('Comp 232', 4, 7, 1);
+insert into Grades values('Comp 232', 4, 8, 1);
+insert into Grades values('Comp 232', 4, 9, .9);
+insert into Grades values('Comp 232', 4, 10, .94);
+insert into Grades values('Comp 232', 4, 11, .80);
+insert into Grades values('Comp 232', 4, 12, 1.03);
+
+insert into Grades values('Comp 232', 5, 7, .95);
+insert into Grades values('Comp 232', 5, 8, 1.5);
+insert into Grades values('Comp 232', 5, 9, .89);
+insert into Grades values('Comp 232', 5, 10, .98);
+insert into Grades values('Comp 232', 5, 11, .89);
+insert into Grades values('Comp 232', 5, 12, .99);
+
+insert into Grades values('Comp 232', 6, 7, .69);
+insert into Grades values('Comp 232', 6, 8, .72);
+insert into Grades values('Comp 232', 6, 9, .54);
+insert into Grades values('Comp 232', 6, 10, .87);
+insert into Grades values('Comp 232', 6, 11, .9);
+insert into Grades values('Comp 232', 6, 12, 1);
+
+
+
 commit;
